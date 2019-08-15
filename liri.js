478ae1fbf0626beb2cdaa4766b7c9beb85c86db4
Nick.Haer <nhaer123@gmail.com>
2019-08-15 17:33:44 -0500

    addes spotify feature

--- liri.js
+++ liri.js
@@ -13,6 +13,8 @@ const spotify = new Spotify(keys.spotify);
 
 let command = process.argv[2]
 
+let param = process.argv[3]
+
 
 
 
@@ -20,33 +22,44 @@ console.log(command)
 
 switch (command) {
     case `concert-this`:
-    let param = process.argv[3]
-    console.log(param)
-    let queryurl = `https://rest.bandsintown.com/artists/${param}/events?app_id=codingbootcamp`
-    axios.get(queryurl).then((response) => {
-        console.log(response.data)
-        console.log(response.data[0].venue.name)
-        console.log(response.data[0].venue.city, response.data[0].venue.region)
-        console.log(moment(response.data[0].datetime).format("MM/DD/YYYY"))
-    }).catch((err) => {
-        console.log(err)
-        console.log(`boogaloo`)
-    })
+        console.log(param)
+        let queryurl = `https://rest.bandsintown.com/artists/${param}/events?app_id=codingbootcamp`
+        axios.get(queryurl).then((response) => {
+            console.log(response.data)
+            console.log(response.data[0].venue.name)
+            console.log(response.data[0].venue.city, response.data[0].venue.region)
+            console.log(moment(response.data[0].datetime).format("MM/DD/YYYY"))
+        }).catch((err) => {
+            console.log(err)
+            console.log(`boogaloo`)
+        })
 
-    break;
+        break;
     case `spotify-this-song`:
+        let song = param
+        spotify
+        .search({ type: 'track', query: song })
+        .then(function(response) {
+            console.log(response.tracks.items[0].album.artists[0].name);
+            console.log(response.tracks.items[0].album.name);
+            console.log(response.tracks.items[0].album.href);
+            console.log(response.tracks.items[0].name)
+        })
+        .catch(function(err) {
+            console.log(err);
+        });
 
 
-    break;
+        break;
     case `movie-this`:
 
 
-    break;
+        break;
     case `do-what-it-says`:
 
 
-    break;
+        break;
 
-    
+
 }
 
